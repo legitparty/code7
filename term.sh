49306afbb4d1e3c69899375b2daa486c2b05daf8
Benjamin Woolley <ben@Benjamins-MacBook.local>
2016-05-15 14:15:36 -0700

    remove need to specify absolute path

--- term.sh
+++ term.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-xset fp+ "$(dirname "${0}")"
+dir="$(dirname "${0}")"
+if [ "${dir}" = . ]
+then
+	dir="$(pwd)/${dir}"
+fi
+xset fp+ "${dir}"
 fc-cache flush
 cp Xresources.code7 ~/.Xresources
 xrdb -merge ~/.Xresources
