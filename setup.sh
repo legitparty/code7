67340720414b68cbd20c2901e9c2fcc1b4f899b5
Benjamin Woolley <ben@prie.st>
2020-09-12 14:47:01 -0700

    attempt to load ttf fonts

--- setup.sh
+++ setup.sh
@@ -2,9 +2,9 @@
 dir="$(dirname "${0}")"
 if [ "${dir}" = . ]
 then
-	dir="$(pwd)/${dir}"
+	dir="$(pwd -L)/${dir}"
 fi
 xset fp+ "${dir}"
-fc-cache flush
+fc-cache -v -f "${dir}"
 xrdb -merge "${dir}/Xresources.code7"
 xrdb -merge "${dir}/Xresources.dark"
